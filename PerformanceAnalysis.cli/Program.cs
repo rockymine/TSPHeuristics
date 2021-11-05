2578990d6c1292709bca4c217685d45a2737b730
michael <michael.g186@live.de>
2021-11-05 19:49:23 +0100

    Add Projects to do Performance Analysis; Add Method to Serialize Graph.

---
+++ PerformanceAnalysis.cli/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using TravellingSalesmanProblem.Algorithms;
+using TravellingSalesmanProblem.Graph;
+using Utility.CommandLine;
+
+namespace PerformanceAnalysis.cli {
+    internal class Program {
+        [Argument('x', "graphSizeX")]
+        private static int GraphSizeX { get; set; }
+        [Argument('y', "graphSizeY")]
+        private static int GraphSizeY { get; set; }
+        [Argument('c', "graphNodeCount")]
+        private static int GraphNodeCount { get; set; }
+        [Argument('h', "heuristic")]
+        private static string Heuristic { get; set; }
+        [Argument('g', "graph")]
+        private static string Graph { get; set; }
+        [Argument('i', "phase")]
+        private static int PhaseLength { get; set; }
+        [Argument('s', "start")]
+        private static double StartTemp { get; set; }
+        [Argument('m', "min")]
+        private static double MinTemp { get; set; }
+        [Argument('a', "alpha")]
+        private static double Alpha { get; set; }
+        //worst case string tryparse
+        [Argument('n', "neighbor")]
+        private static NeighbourType NeighbourEnum { get; set; }
+        [Argument('p', "path")]
+        private static string Path { get; set; }
+        private static LinkedList<GraphState> History { get; set; }
+        private static void Main(string[] args) {
+            Arguments.Populate();
+
+            var instance = GraphProblem.RandomGraphProblem(GraphSizeX, GraphSizeY, GraphNodeCount);
+            Stopwatch sw = new();
+            sw.Restart();
+            switch (Heuristic.ToLower()) {
+                case "sa":
+                case "annealing":
+                    SimulatedAnnealing sa = new();
+                    sa.Alpha = Alpha;
+                    sa.MinTemp = MinTemp;
+                    sa.PhaseLength = PhaseLength;
+                    sa.StartTemp = StartTemp;
+                    sa.NeighbourEnum = NeighbourEnum;
+                    History = sa.FindPath(instance);
+                    break;
+            }
+
+            sw.Stop();
+
+            var result = new SimulationResult {
+                GraphSizeX = GraphSizeX,
+                GraphSizeY = GraphSizeY,
+                GraphNodeCount = GraphNodeCount,
+                Heuristic = Heuristic,
+                PhaseLength = PhaseLength,
+                StartTemp = StartTemp,
+                MinTemp = MinTemp,
+                Alpha = Alpha,
+                NeighbourEnum = NeighbourEnum,
+                RunTime = sw.Elapsed.ToString(),
+                TourLength = History.Last.Value.Distance
+            };
+
+            File.WriteAllText(Path, JsonConvert.SerializeObject(result));
+        }
+    }
+}
---
+++ PerformanceAnalysis.cli/SimulationResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravellingSalesmanProblem.Algorithms;
+
+namespace PerformanceAnalysis.cli {
+    public class SimulationResult {
+        public int GraphSizeX { get; set; }
+        public int GraphSizeY { get; set; }
+        public int GraphNodeCount { get; set; }
+        public string Heuristic { get; set; }
+        public int PhaseLength { get; set; }
+        public double StartTemp { get; set; }
+        public double MinTemp { get; set; }
+        public double Alpha { get; set; }
+        public NeighbourType NeighbourEnum { get; set; }
+        public string RunTime { get; set; }
+        public double TourLength { get; set; }
+    }
+}
