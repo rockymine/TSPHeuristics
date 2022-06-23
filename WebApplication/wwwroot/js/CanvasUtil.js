387eb5a1ed2edeb6a18ee5156cac016df77653dc
michael <michael.g186@live.de>
2022-06-23 22:42:19 +0200

    Update UI; Add Help (WIP); Add Modal for full size table

---
+++ WebApplication/wwwroot/js/CanvasUtil.js
@@ -0,0 +1,14 @@
+﻿window.addEventListener("resize", SetCanvases);
+
+function SetCanvases(e) {
+    var canvases = document.getElementsByClassName("CanvasFullSize");
+    for (var i = 0; i < canvases.length; i++) {
+        var canvas = canvases[i];
+        canvas.width = canvas.parentElement.offsetWidth;
+        canvas.height = canvas.parentElement.offsetHeight;
+    }
+}
+
+function UpdateCanvases() {
+    SetCanvases(null);
+}
